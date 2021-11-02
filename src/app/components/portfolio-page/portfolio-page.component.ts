f938cca53bab41cd31b135531f7fcfa935cb3a8e
Emily Tracy <emgoheen@yahoo.com>
2021-11-02 16:02:57 -0700

    Fixed scss path errors

--- src/app/components/portfolio-page/portfolio-page.component.ts
+++ src/app/components/portfolio-page/portfolio-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-portfolio-page',
   templateUrl: './portfolio-page.component.html',
-  styleUrls: ['./portfolio-page.component.css']
+  styleUrls: ['./portfolio-page.component.scss']
 })
 export class PortfolioPageComponent implements OnInit {
 
